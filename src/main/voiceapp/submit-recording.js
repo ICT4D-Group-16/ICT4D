20784353ca06755ebfbbfd643293f28486c7b8f5
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-05-02 21:07:43 +0200

    Update submit-recording.js

--- src/main/voiceapp/submit-recording.js
+++ src/main/voiceapp/submit-recording.js
@@ -4,5 +4,5 @@ function submit_recording(name, data) {
     const request = new XMLHttpRequest();
     request.open("POST", "tst0.herokuapp.com/upload");
     request.send(form_data);
-    return 0;
+    return ("submit_recording function will immediately return");
 }
