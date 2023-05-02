51798f2e4c067dfa8e41b4f17f7509fdc11c06bf
zhiheng-yang <young-z-h@outlook.com>
2023-05-02 19:13:44 +0200

    ALl pms CRUD and passed http tests

--- src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderDetailController.java
+++ src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderDetailController.java
@@ -1,16 +1,9 @@
 package com.ict4d_16.dos.modules.pms.controller;
 
 
-import com.ict4d_16.dos.common.api.CommonResult;
 import com.ict4d_16.dos.modules.pms.model.PmsOrderDetail;
 import com.ict4d_16.dos.modules.pms.service.PmsOrderDetailService;
-import com.ict4d_16.dos.modules.ums.dto.UmsAdminParam;
-import com.ict4d_16.dos.modules.ums.model.UmsAdmin;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +13,8 @@ import java.util.List;
  * Order detail table. Each order has N order detail table. 前端控制器
  * </p>
  *
- * @author macro
  * @since 2023-05-01
+ * @version 1.0
  */
 @RestController
 @RequestMapping("/pms/pmsOrderDetail")
--- src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderMasterController.java
+++ src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderMasterController.java
@@ -1,21 +1,53 @@
 package com.ict4d_16.dos.modules.pms.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.ict4d_16.dos.modules.pms.model.PmsOrderMaster;
+import com.ict4d_16.dos.modules.pms.service.PmsOrderMasterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
  * Order Info Table 前端控制器
  * </p>
  *
- * @author macro
  * @since 2023-05-01
+ * @version 1.0
  */
 @RestController
 @RequestMapping("/pms/pmsOrderMaster")
 public class PmsOrderMasterController {
+    @Autowired
+    private PmsOrderMasterService pmsOrderMasterService;
 
+    @PostMapping("/add")
+    public String add(@RequestBody PmsOrderMaster pmsOrderMaster) {
+        boolean success = pmsOrderMasterService.save(pmsOrderMaster);
+        return success ? "success" : "failure";
+    }
+
+    @GetMapping("/list")
+    public List<PmsOrderMaster> list() {
+        return pmsOrderMasterService.list();
+    }
+
+    @GetMapping("/get/{id}")
+    public PmsOrderMaster getById(@PathVariable Long id) {
+        return pmsOrderMasterService.getById(id);
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody PmsOrderMaster pmsOrderMaster) {
+        boolean success = pmsOrderMasterService.updateById(pmsOrderMaster);
+        return success ? "success" : "failure";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        boolean success = pmsOrderMasterService.removeById(id);
+        return success ? "success" : "failure";
+    }
 }
 
