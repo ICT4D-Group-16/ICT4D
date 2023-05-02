c6ed0a52d9d70b6beda0f426791efaae92077ec9
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-05-02 15:34:06 +0200

    adding record

---
+++ src/main/voiceapp/submit-recording.js
@@ -0,0 +1,8 @@
+function submit_recording(name, data) {
+    const form_data = new FormData();
+    form_data.append(name, data);
+    const request = new XMLHttpRequest();
+    request.open("POST", "tst0.herokuapp.com/upload");
+    request.send(form_data);
+    return 0;
+}
