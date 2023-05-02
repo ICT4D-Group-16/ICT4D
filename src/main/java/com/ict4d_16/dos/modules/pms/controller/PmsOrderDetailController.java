062cdaebd447a9933f6c8e836de06e4c21cc7853
zhiheng-yang <young-z-h@outlook.com>
2023-05-02 18:58:23 +0200

    CRUD samples and passed http tests

--- src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderDetailController.java
+++ src/main/java/com/ict4d_16/dos/modules/pms/controller/PmsOrderDetailController.java
@@ -2,13 +2,18 @@ package com.ict4d_16.dos.modules.pms.controller;
 
 
 import com.ict4d_16.dos.common.api.CommonResult;
+import com.ict4d_16.dos.modules.pms.model.PmsOrderDetail;
+import com.ict4d_16.dos.modules.pms.service.PmsOrderDetailService;
 import com.ict4d_16.dos.modules.ums.dto.UmsAdminParam;
 import com.ict4d_16.dos.modules.ums.model.UmsAdmin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -21,6 +26,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/pms/pmsOrderDetail")
 public class PmsOrderDetailController {
+    @Autowired
+    private PmsOrderDetailService pmsOrderDetailService;
 
+    @PostMapping("/add")
+    public String add(@RequestBody PmsOrderDetail pmsOrderDetail) {
+        boolean success = pmsOrderDetailService.save(pmsOrderDetail);
+        return success ? "success" : "failure";
+    }
+
+    @GetMapping("/list")
+    public List<PmsOrderDetail> list() {
+        return pmsOrderDetailService.list();
+    }
+
+    @GetMapping("/get/{id}")
+    public PmsOrderDetail getById(@PathVariable Long id) {
+        return pmsOrderDetailService.getById(id);
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody PmsOrderDetail pmsOrderDetail) {
+        boolean success = pmsOrderDetailService.updateById(pmsOrderDetail);
+        return success ? "success" : "failure";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        boolean success = pmsOrderDetailService.removeById(id);
+        return success ? "success" : "failure";
+    }
 }
 
