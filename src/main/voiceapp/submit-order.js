8b19e8ac4750e8b053fe03b1e5b36cd80fbd0613
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-05-17 12:49:47 +0200

    Update submit-order.js

--- src/main/voiceapp/submit-order.js
+++ src/main/voiceapp/submit-order.js
@@ -1,6 +1,6 @@
 function submit_order_master() {
-    return ("called submit_order_master()")
+    return ("Added.")
 }
 function add_order_detail() {
-    return ("called add_order_detail()")
+    return ("You submitted all the products added.")
 }
